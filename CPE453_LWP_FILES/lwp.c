c8c447e87ce5846ae6d347d82073dd6a7012d87b
Noah Weitzel <noah@LAPTOP-8GF9TQRC>
2025-04-23 22:10:11 -0700

    debugging comments more

--- CPE453_LWP_FILES/lwp.c
+++ CPE453_LWP_FILES/lwp.c
@@ -6,7 +6,7 @@ lwp_context lwp_ptable[LWP_PROC_LIMIT];
 int lwp_procs = 0;
 int lwp_running = -1;
 
-static ptr_int_t *main_sp = NULL;
+// static ptr_int_t *main_sp = NULL;
 
 int new_lwp(lwpfun fun, void *arg, size_t stacksize) {
     if (lwp_procs >= LWP_PROC_LIMIT) return -1;
@@ -15,23 +15,26 @@ int new_lwp(lwpfun fun, void *arg, size_t stacksize) {
     ptr_int_t *stack = malloc(stacksize * sizeof(ptr_int_t));
     if (!stack) return -1;
 
-    ptr_int_t *sp = stack + stacksize;  // top of stack (stack grows down)
+    ptr_int_t *sp = stack + stacksize;
 
-    *(--sp) = (ptr_int_t)arg;               // push argument to indentnum
-    *(--sp) = (ptr_int_t)lwp_exit;          // fake return addr to call lwp_exit when done
-    *(--sp) = (ptr_int_t)fun;               // this acts like a "return address" to jump to
+    *(--sp) = (ptr_int_t)arg;        // argument to pass
+    *(--sp) = (ptr_int_t)lwp_exit;   // return address after thread finishes
+    *(--sp) = (ptr_int_t)fun;        // "fake" return to thread function
 
-    // Push dummy register values (eax, ebx, ecx...) in reverse order of RESTORE_STATE
-    for (int i = 0; i < 6; i++) *(--sp) = 0;  // edi, esi, edx, ecx, ebx, eax
-    *(--sp) = (ptr_int_t)(sp + 1);           // fake ebp
+    int i;
+    for (i = 0; i < 6; i++) *(--sp) = 0;  // dummy edi, esi, edx, ecx, ebx, eax
+
+    sp--;
+    *sp = (ptr_int_t)(sp + 1);  // fake ebp
+
 
     proc->pid = lwp_procs;
     proc->stack = stack;
     proc->stacksize = stacksize;
     proc->sp = sp;
 
-    printf("[DEBUG] Created LWP %d, stack base = %p, sp = %p\n",
-        proc->pid, (void*)stack, (void*)sp);
+    printf("[DEBUG] Created LWP %lu, stack base = %p, sp = %p\n", (unsigned long)proc->pid, (void*)stack, (void*)sp);
 
     return lwp_procs++;
 }
+
